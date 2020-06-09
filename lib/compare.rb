6a583f21263fc328a9a69e67667197a8c930d81a
plkujaw <jawor.jakub@gmail.com>
2020-06-09 11:45:29 +0100

    added tests from kata to check and it works regardless it fails on kata

--- lib/compare.rb
+++ lib/compare.rb
@@ -1,7 +1,14 @@
 def compare(a, b)
-  if ((a == nil || b == nil) || (a.empty? || b.empty?))
+  if (a == nil || b == nil)
     return false
+  elsif (a.empty? || b.empty?)
+    return true
   else
     a.map { |e| e**2 }.sum == b.sum
   end
 end
+
+
+# this solution doesnt pass kata's tests
+# for some reason, if I replace .sum with .sort it does work
+# when testing kata's examples in rspec, my solution works
--- spec/compare_spec.rb
+++ spec/compare_spec.rb
@@ -1,23 +1,27 @@
 require "compare"
 
-describe "#comp" do
+describe "#compare" do
   it "it compares if element of array b is a square of element of array a" do
     expect(compare([2], [4])).to eq(true)
     expect(compare([1], [4])).to eq(false)
   end
 
-  it "compares if elements in array b are equal to elements in array a squares" do
+  it "compares if elements in array b are equal to elements in array a squared" do
     expect(compare([1, 2], [1, 4])).to eq(true)
+    expect(compare([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361])).to eq(true)
+    expect(compare([121, 144, 19, 161, 19, 144, 19, 11], [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19])).to eq(true)
+    expect(compare([121, 144, 19, 161, 19, 144, 19, 11], [132, 14641, 20736, 361, 25921, 361, 20736, 361])).to eq(false)
+    expect(compare([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 36100, 25921, 361, 20736, 361])).to eq(false)
     expect(compare([2, 2], [1, 4])).to eq(false)
   end
 
   it "returns false if any array is empty" do
-    expect(compare([1], [])).to eq(false)
-    expect(compare([1, 4, 7], [])).to eq(false)
+    expect(compare([1], [])).to eq(true)
+    expect(compare([1, 4, 7], [])).to eq(true)
   end
 
   it "returns false if both arrays are empty" do
-    expect(compare([], [])).to eq(false)
+    expect(compare([], [])).to eq(true)
   end
 
   it "returns false if any of arrays is nil" do
