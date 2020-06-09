c05bb4afc4494496a102cfc7d0b1d09502f83b7b
plkujaw <jawor.jakub@gmail.com>
2020-06-09 09:50:52 +0100

    add and pass first false test for arrays with 1 element

--- spec/compare_spec.rb
+++ spec/compare_spec.rb
@@ -3,5 +3,6 @@ require "compare"
 describe "#comp" do
   it "it compares if element of array b is a square of element of array a" do
     expect(compare([2], [4])).to eq(true)
+    expect(compare([1], [4])).to eq(false)
   end
 end
